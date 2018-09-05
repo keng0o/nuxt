163ca19cfeb261dda571c12155e6a8151a3ce74a
黒田学人 <kuroda.manato@vega-c.com>
2018-09-05 19:28:29 +0900

    refactor: move header to layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
+    <TheHeader />
     <nuxt />
   </div>
 </template>
 
 <style>
 </style>
+<script>
+import TheHeader from '@/components/TheHeader'
+export default {
+  components: { TheHeader },
+}
+</script>
