35c64640c0e1fc35de570b93db56501b23a6ec58
黒田学人 <kuroda.manato@vega-c.com>
2018-09-05 19:16:54 +0900

    feat: add header

---
+++ components/VIcon.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="icon-wrap">
+    <svg
+      :width="width"
+      :height="height"
+      :aria-labelledby="name"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      role="presentation"
+    >
+      <g :fill="color">
+        <component :is="name" />
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script>
+import IconSearch from '@/assets/svg/IconSearch'
+import IconFavorite01 from '@/assets/svg/IconFavorite01'
+import IconFavorite02 from '@/assets/svg/IconFavorite02'
+import IconCart from '@/assets/svg/IconCart'
+
+export default {
+  components: {
+    search: IconSearch,
+    favorite01: IconFavorite01,
+    favorite02: IconFavorite02,
+    cart: IconCart,
+  },
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    width: {
+      type: [Number, String],
+      default: 20,
+    },
+    height: {
+      type: [Number, String],
+      default: 20,
+    },
+    color: {
+      type: String,
+      default: 'currentColor',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.icon-wrap {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+svg {
+  display: inline-block;
+  color: #2a3139;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,86 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo />
-      <h1 class="title">
-        my-nuxt
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+  <div class="home">
+    <header class="header container">
+      <div class="header-Logo">
+        <nuxt-link :to="{}">
+          <img
+            :src="Logo"
+            width="100"
+            height="100"
+          >
+        </nuxt-link>
       </div>
-    </div>
-  </section>
+      <nav class="header-Nav">
+        <ul>
+          <li>
+            <nuxt-link :to="{}">item</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link :to="{}">coordinate</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link :to="{}">magazine</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link :to="{}">about</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link :to="{}">information</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link :to="{}">user guide</nuxt-link>
+          </li>
+        </ul>
+
+        <div class="header-SearchBox">
+          <form>
+            <VIcon
+              name="search"
+              class="searchIcon" />
+            <input
+              :class="{ focused: isFocusedSearch }"
+              type="search"
+              class="search"
+              placeholder="Search for"
+              @focus="isFocusedSearch = true"
+              @blur="isFocusedSearch = false"
+            >
+            <input
+              type="submit"
+              value="submit">
+          </form>
+        </div>
+
+        <div class="header-Account">
+          <ul>
+            <li>
+              <nuxt-link :to="{}">sign in</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link :to="{}">
+                <VIcon name="favorite01" />
+              </nuxt-link>
+
+            </li>
+            <li>
+              <nuxt-link :to="{}">
+                <VIcon name="cart" />
+              </nuxt-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  </div>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
 import Home from '@/graphql/Home.graphql'
+import Logo from '@/assets/images/logo.svg'
+import VIcon from '@/components/VIcon'
 
 export default {
-  components: {
-    AppLogo,
-  },
+  components: { VIcon },
 
   apollo: {
     allUsers: {
@@ -37,37 +88,129 @@ export default {
       loadingKey: 'loading',
     },
   },
+
+  data() {
+    return {
+      isFocusedSearch: false,
+    }
+  },
+
+  created() {
+    this.Logo = Logo
+  },
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 .container {
-  min-height: 100vh;
+  width: 1440px;
+  margin: 0 auto;
+}
+
+.header {
+  height: 100px;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  padding: 0 40px;
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  &-Nav {
+    flex: 1;
+    display: flex;
+    justify-content: flex-end;
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+    > ul {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 100%;
+    }
 
-.links {
-  padding-top: 15px;
+    > ul > li {
+      &:last-of-type {
+        margin-right: 15px;
+      }
+    }
+
+    > ul > li > a {
+      padding: 15px;
+
+      font-size: 1.6rem;
+      font-family: 'Roboto';
+      font-weight: 600;
+      color: #000;
+      text-transform: uppercase;
+      letter-spacing: 0.05em;
+    }
+  }
+
+  &-SearchBox {
+    display: flex;
+    align-items: center;
+    margin-right: 20px;
+
+    > form {
+      position: relative;
+      transition: width 0.25s;
+
+      .searchIcon {
+        position: absolute;
+        top: 50%;
+        left: 10px;
+        margin-top: -10px;
+        font-size: 1.6rem;
+      }
+    }
+    .search {
+      width: 210px;
+      font-family: 'Roboto';
+      font-size: 1.6rem;
+      padding: 10px 10px 10px 40px;
+      border: 1px solid #ddd;
+
+      transition: width 0.25s, border 0.25s;
+
+      &.focused {
+        border: 1px solid #2f2f2f;
+      }
+    }
+
+    input[type='submit'] {
+      display: none;
+    }
+  }
+
+  &-Account {
+    display: flex;
+    align-items: center;
+
+    ul {
+      display: flex;
+      align-items: center;
+    }
+
+    ul > li {
+      margin-right: 16px;
+    }
+
+    ul > li > a {
+      font-size: 1.1rem;
+      font-weight: 700;
+      text-transform: uppercase;
+      color: #909090;
+      transition: opacity 0.25s;
+      letter-spacing: 0.05em;
+
+      /deep/ svg {
+        transition: color 0.25s;
+      }
+
+      &:hover {
+        opacity: 0.8;
+
+        /deep/ svg {
+          color: #9bcbeb;
+        }
+      }
+    }
+  }
 }
 </style>
